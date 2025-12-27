d3defbd7b83d46609d8baa5e1e8a572088647937
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-12-26 21:55:53 -0500

    work on fi for empty lists outside of read function

--- reading.js
+++ reading.js
@@ -3,44 +3,34 @@
 //const readline = require('readline');
 
 
-
+let dataLines = []; 
 let kanjiList = [];//to hold kanji chars in order
-const meaningsList = []; //holds the meanings; indices correspond to the kanji 
-const readingsList = []; //stored "on;kun"
+let meaningsList = []; //holds the meanings; indices correspond to the kanji 
+let readingsList = []; //stored "on;kun"
 
 
 
 fetch("https://raw.githubusercontent.com/alisherturakulov/language-app/refs/heads/main/kanji.csv")
     .then(response => response.text())
-    .then(data => {
-       // console.log(data)
-        const dataLines = data.split('\n');
-        dataLines[dataLines.length-1] = " "; //to avoid empty last index
-        //console.log(dataLines)
-        
-        // for(let i = 0; i< dataLines.length; i++){
-        //     dataLines[i] = dataLines[i].split(',');
-        // }
-        //dataLines = dataLines.map(ln => ln.split(','));
-        for (let i = 0; i<dataLines.length; i++){
-            const row=  dataLines[i].split(',');
-            console.log(row);
-            if(!isNaN(row[0])){
-                kanjiList.push(row[1]);
-                meaningsList.push(row[7]);
-                readingsList.push(row[8]+";"+row[9]);
-            }
-            
-        }
-        console.log(kanjiList);
-        console.log(meaningsList);
-        console.log(readingsList);
+    .then((data) => {
+        //console.log(data);
+       // dataLines = data;
+
+       
+      const lists = readCSVString(data);
+      console.log(lists);
+      kanjiList = lists.kanji;
+      meaningsList = lists.meanings;
+      readingsList = lists.readings;
         
     })
     .catch(error => console.error('Error fetching file: ', error));
-console.log(kanjiList);
+
 //readCSV("kanji.csv", kanjiList, meaningsList, readingsList);
-//console.log(kanji);
+//readCSVString(dataLines, kanjiList, meaningsList, readingsList);
+
+console.log(kanjiList);
+console.log(dataLines);
 
 // let currentLetterIndex = 0;
 // changeLetterCard(currentLetterIndex, kanjiList, meaningsList, readingsList);
@@ -54,6 +44,43 @@ console.log(kanjiList);
 //     currentLetterIndex = Math.min(kanjiList.length, currentLetterIndex+1);
 //     changeLetterCard(currentLetterIndex, kanji, meaningsList, readingsList);
 // });
+/**
+ * reads and processes csv file data
+ * @param {string} data holding the string represenation of the file
+ */
+async function readCSVString(data){
+    const kanji = [];
+    const meanings = [];
+    const readings = [];
+    dataLines = data.split('\n');
+        dataLines[dataLines.length-1] = " "; //to avoid empty last index
+        //console.log(dataLines)
+        
+        // for(let i = 0; i< dataLines.length; i++){
+        //     dataLines[i] = dataLines[i].split(',');
+        // }
+        //dataLines = dataLines.map(ln => ln.split(','));
+        for (let i = 0; i<dataLines.length; i++){
+            const row=  dataLines[i].split(',');
+            //console.log(row);
+            if(!isNaN(row[0])){
+                kanji.push(row[1]);
+                meanings.push(row[7]);
+                readings.push(row[8]+";"+row[9]);
+            }
+            
+        }
+       
+        return {
+            kanji,
+            meanings,
+            readings
+        };
+       
+}
+
+
+
 
 // /*
 //  function readCSV(fname, kanji, meanings, readings){
