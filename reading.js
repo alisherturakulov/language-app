adea24a3077780600637ee70b2ef5085bc5327f3
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-12-30 23:42:37 -0500

    add styling to conversation page

--- reading.js
+++ reading.js
@@ -1,6 +1,6 @@
 
-//const fs = require('fs');
-//const readline = require('readline');
+const fs = require('fs');//filestream
+const readline = require('readline');//for file reading
 
 
 let dataLines = []; 
@@ -8,28 +8,33 @@ let kanjiList = [];//to hold kanji chars in order
 let meaningsList = []; //holds the meanings; indices correspond to the kanji 
 let readingsList = []; //stored "on;kun"
 
+// await readCSV(kanjiList, meaningsList, readingsList);
+
+// async function readCSV(kanji, meanings, readings){
+//  fetch("https://raw.githubusercontent.com/alisherturakulov/language-app/refs/heads/main/kanji.csv")
+//     .then(response => response.text())
+//     .then((data) => {
+//         //console.log(data);
+//        // dataLines = data;
+
+//        dataLines = data;
+//       const lists = readCSVString(data);
+      
+//       console.log(lists);
+//       kanjiList.concat(lists.kanji);
+     
+//         return lists;
+//     })
+//     .catch(error => console.error('Error fetching file: ', error));
+
+//    kanji = listsObj.kanji;
+// }
 
 
-fetch("https://raw.githubusercontent.com/alisherturakulov/language-app/refs/heads/main/kanji.csv")
-    .then(response => response.text())
-    .then((data) => {
-        //console.log(data);
-       // dataLines = data;
+readCSV("kanji.csv", kanjiList, meaningsList, readingsList);
+readCSVString(dataLines, kanjiList, meaningsList, readingsList);
 
-       
-      const lists = readCSVString(data);
-      console.log(lists);
-      kanjiList = lists.kanji;
-      meaningsList = lists.meanings;
-      readingsList = lists.readings;
-        
-    })
-    .catch(error => console.error('Error fetching file: ', error));
-
-//readCSV("kanji.csv", kanjiList, meaningsList, readingsList);
-//readCSVString(dataLines, kanjiList, meaningsList, readingsList);
-
-console.log(kanjiList);
+console.log(listsObj.kanji);
 console.log(dataLines);
 
 // let currentLetterIndex = 0;
@@ -44,11 +49,12 @@ console.log(dataLines);
 //     currentLetterIndex = Math.min(kanjiList.length, currentLetterIndex+1);
 //     changeLetterCard(currentLetterIndex, kanji, meaningsList, readingsList);
 // });
+
 /**
  * reads and processes csv file data
  * @param {string} data holding the string represenation of the file
  */
-async function readCSVString(data){
+ function readCSVString(data){
     const kanji = [];
     const meanings = [];
     const readings = [];
@@ -82,63 +88,63 @@ async function readCSVString(data){
 
 
 
-// /*
-//  function readCSV(fname, kanji, meanings, readings){
-//     const fileStream = fs.createReadStream(fname);
 
-//     const rl = readline.createInterface({
-//         input: fileStream,
-//         crlfDelay: Infinity
-//     });
+ function readCSV(fname, kanji, meanings, readings){
+    const fileStream = fs.createReadStream(fname);
 
-//     let tableLine = [];//csv file line
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let tableLine = [];//csv file line
     
-//   //  for await (const line of rl){
-//   //      tableLine = line.split(',');     
-//   //  }
+  //  for await (const line of rl){
+  //      tableLine = line.split(',');     
+  //  }
 
     
-//     rl.on('line', (line) => {
-//         tableLine = line.split(',');
-//         if(!isNaN(tableLine[0])){
-//             kanji.push(tableLine[1]);
-//             meanings.push(tableLine[7]);
-//             readings.push(tableLine[8]+";"+tableLine[9]);
-//         }//else{
-//           //  console.log("header: " + tableLine[0]);//should be index
-//       //  }
-//     });
+    rl.on('line', (line) => {
+        tableLine = line.split(',');
+        if(!isNaN(tableLine[0])){
+            kanji.push(tableLine[1]);
+            meanings.push(tableLine[7]);
+            readings.push(tableLine[8]+";"+tableLine[9]);
+        }//else{
+          //  console.log("header: " + tableLine[0]);//should be index
+      //  }
+    });
     
 
-//     rl.on('close', () => {
-//         //console.log(kanji); //works
-//     });
+    rl.on('close', () => {
+        //console.log(kanji); //works
+    });
 
-// }*/
+}
 
 
 
-// //function readIntoTable(fileName){}
+//function readIntoTable(fileName){}
 
-// /**
-//  * changes the letter card displayed
-//  * @param {integer} letterIndex index of kanjiList
-//  * @throws Error when letter isNaN
-//  */
-// function changeLetterCard(letterIndex, kanji, meanings, readings){
-//     if(isNaN(letterIndex) || letterIndex < 0 || letterIndex >= kanji.length){
-//        // throw new Error("invalid letter index");
-//     }
-//     const letter = document.getElementById("kanjiCharacter");
-//     const meaning = document.getElementById("meaning");
-//     const onReading = document.getElementById("onReading");
-//     const kunReading= document.getElementById("kunReading");
+/**
+ * changes the letter card displayed
+ * @param {integer} letterIndex index of kanjiList
+ * @throws Error when letter isNaN
+ */
+function changeLetterCard(letterIndex, kanji, meanings, readings){
+    if(isNaN(letterIndex) || letterIndex < 0 || letterIndex >= kanji.length){
+       // throw new Error("invalid letter index");
+    }
+    const letter = document.getElementById("kanjiCharacter");
+    const meaning = document.getElementById("meaning");
+    const onReading = document.getElementById("onReading");
+    const kunReading= document.getElementById("kunReading");
     
-//     letter.innerText = kanji[letterIndex];
-//     meaning.innerText = meanings[letterIndex];
+    letter.innerText = kanji[letterIndex];
+    meaning.innerText = meanings[letterIndex];
    
-//     onReading.innerText = readings[letterIndex].substring(0, readings[letterIndex].indexOf(';'));
-//     kunReading.innerText = readings[letterIndex].substring(readings[letterIndex].indexOf(';')+1);
-//     console.log("current index: " + letterIndex)
-// }
+    onReading.innerText = readings[letterIndex].substring(0, readings[letterIndex].indexOf(';'));
+    kunReading.innerText = readings[letterIndex].substring(readings[letterIndex].indexOf(';')+1);
+    console.log("current index: " + letterIndex)
+}
 
